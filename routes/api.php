5bb1910e56731de27da76ecaa730b49fd9f04847
Marco <marcoregina92@gmail.com>
2024-02-08 17:20:26 +0100

    change route events

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/prova", [EventController::class, "index"]);
+Route::get("/events", [EventController::class, "index"]);
